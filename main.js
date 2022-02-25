d1192ab9f0e824583c978e56c507789737d9db82
Haleem001 <73344166+Haleem001@users.noreply.github.com>
2022-02-25 16:23:42 +0100

    Update main.js

--- main.js
+++ main.js
@@ -11,3 +11,21 @@ function createMeteors(){
 }
 
 setInterval(createMeteors,100);
+
+var mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
